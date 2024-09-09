f9c6048691c092b024e0e237473f3dab1dc48078
Damir Safarov <dsafarov@ipgphotonics.com>
2024-09-09 16:23:30 +0200

    added domain join script

---
+++ Setup-HypervADDomain/Join-Domain.ps1
@@ -0,0 +1,37 @@
+# Prepare NIC
+$NICName = (Get-NetAdapter)[0].Name
+
+$InterfaceAlias = $NICName
+$IPAddress = "10.0.20.101"
+$PrefixLength = 24
+$DefaultGateway = "10.0.20.1"
+$DNS1 = "10.0.20.10"
+
+New-NetIPAddress `
+    -InterfaceAlias $InterfaceAlias `
+    -IPAddress $IPAddress `
+    -PrefixLength $PrefixLength `
+    -DefaultGateway $DefaultGateway
+
+Set-DnsClientServerAddress `
+    -InterfaceAlias $InterfaceAlias `
+    -ServerAddresses $DNS1
+
+# Define variables
+$NewComputerName = "TCL01"  # Specify the new name for the computer
+$DomainName = "target.local"          # Specify the domain to join
+$DomainUser = "dutyadmin"          # Specify the domain admin username
+$DomainPassword = "BArakuda@123"      # Specify the domain admin password
+
+# Create a secure string for the password
+$SecurePassword = ConvertTo-SecureString $DomainPassword -AsPlainText -Force
+
+# Create a PSCredential object
+$Credential = New-Object System.Management.Automation.PSCredential ($DomainUser, $SecurePassword)
+
+# Join the computer to the domain
+Start-Sleep -Seconds 10
+Add-Computer -NewName $NewComputerName -DomainName $DomainName -Credential $Credential
+
+# Optional: Restart the computer after joining the domain
+Restart-Computer -Force
