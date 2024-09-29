2b48858b6468bb63d182f5b1bd44c8cee8bc6523
damup1988@gmail.com <damup1988@gmail.com>
2024-09-29 18:05:24 +0200

    added ADMT prep script

--- Setup-HypervADDomain/Setup-Env.ps1
+++ Setup-HypervADDomain/Setup-Env.ps1
@@ -5,20 +5,35 @@ New-NetIPAddress -InterfaceAlias "vEthernet $switchName" -IPAddress 10.0.10.1 -P
 
 
 # Win Server 2022 Std
-$NewVMName = "VM10"
+$NewVMName = "VM11"
 $VHDXFolder = "D:\_VMs"
 $SourceVHDXPath = "$VHDXFolder\IMAGEWS2022STDGEN1.vhdx"
 $NewVMDisk = Copy-Item -Path $SourceVHDXPath -Destination "$VHDXFolder\$NewVMName.vhdx"
 New-VM `
     -Name $NewVMName `
-    -MemoryStartupBytes 1Gb `
+    -MemoryStartupBytes 2Gb `
     -VHDPath "$VHDXFolder\$NewVMName.vhdx" `
     -SwitchName $switchName
 
+# Win Server 2022 Std Gen2
+$NewVMName = "VM12"
+$VHDXFolder = "D:\_VMs"
+$SourceVHDXPath = "$VHDXFolder\IMAGEWS2022STDGEN1.vhdx"
+$NewVMDisk = Copy-Item -Path $SourceVHDXPath -Destination "$VHDXFolder\$NewVMName.vhdx"
+New-VM `
+    -Name $NewVMName `
+    -MemoryStartupBytes 2Gb `
+    -VHDPath "$VHDXFolder\$NewVMName.vhdx" `
+    -SwitchName $switchName `
+    -Generation 2
 
 # W11 Pro
 $NewVMName = "VM8"
 $VHDXFolder = "D:\_VMs"
 $SourceVHDXPath = "$VHDXFolder\_IMAGEW11ENTGEN2.vhdx"
 $NewVMDisk = Copy-Item -Path $SourceVHDXPath -Destination "$VHDXFolder\$NewVMName.vhdx"
-New-VM -Name $NewVMName -Generation 2 -MemoryStartupBytes 2Gb -VHDPath "$VHDXFolder\$NewVMName.vhdx"
+New-VM `
+    -Name $NewVMName `
+    -Generation 2 `
+    -MemoryStartupBytes 2Gb `
+    -VHDPath "$VHDXFolder\$NewVMName.vhdx"
