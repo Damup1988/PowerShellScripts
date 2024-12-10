0da2d8a32f0a262e20d977fe8741716950f41c2b
Damir Safarov <dsafarov@ipgphotonics.com>
2024-12-10 16:19:34 +0100

    added unattend file for win11

--- Setup-HypervADDomain/Promote-Domain.ps1
+++ Setup-HypervADDomain/Promote-Domain.ps1
@@ -1,14 +1,16 @@
+$DomainPassword = "BArakuda@123"
+$SecurePassword = ConvertTo-SecureString $DomainPassword -AsPlainText -Force
 Import-Module ADDSDeployment
 Install-ADDSForest `
 -CreateDnsDelegation:$false `
 -DatabasePath "C:\Windows\NTDS" `
 -DomainMode "WinThreshold" `
--DomainName "MGMT.local" `
--DomainNetbiosName "MGMT" `
+-DomainName "LAPS.local" `
+-DomainNetbiosName "LAPS" `
 -ForestMode "WinThreshold" `
 -InstallDns:$true `
 -LogPath "C:\Windows\NTDS" `
 -NoRebootOnCompletion:$false `
 -SysvolPath "C:\Windows\SYSVOL" `
--Force:$true
-
+-Force:$true `
+-SafeModeAdministratorPassword $SecurePassword
